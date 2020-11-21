578489d8d087f7cfeb408678353739a2d6593283
Devan-Li <devan.v2v@gmail.com>
2020-11-20 21:38:27 -0500

    following the question to test a list menus

--- calc.sh
+++ calc.sh
@@ -1,13 +1,18 @@
 #! /usr/bin/env bash
 
-# When the program is first loaded, display a greeting to the user.;
+# When the program is first loaded, display a greeting to the user.
 
-echo "Hello, welcome to Script World"
+#echo "Hello, welcome to Script World"
 
 # Then, display a menu that outlines the possible operations:
   # Add
   # Subtract
   # Exit
+
+for action in Add Subtract Exit; do
+  echo $action
+done
+
 # Then, capture the user selection.
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
